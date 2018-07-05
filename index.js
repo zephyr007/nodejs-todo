a87146fab0509a2f54439fd95e96213d74b6d32e
nitin dhemiwal <dhemiwal.nitin@gmail.com>
2018-07-06 00:56:22 +0530

    test run commit #1

--- index.js
+++ index.js
@@ -47,8 +47,8 @@ todoController(app);     // this todoController is in is an imported function. w
 
 //listen to port
 
-app.listen(3000)
+app.listen(3000);
 
-console.log('listening on port 3000')
+console.log('listening on port 3000');
 //app.listen(PORT, () =>console.log(`Listening on ${ PORT }`) );
 
